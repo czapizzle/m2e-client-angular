d5d3a8ece3da71d4266f35d1b3aed768124b7fdc
Pawel Czapik <p.czapik@gmail.com>
2019-06-14 00:16:59 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
+import { CartComponent } from './views/cart/cart.component';
+import { AccountComponent } from './views/account/account.component';
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'account', component: AccountComponent },
+  { path: 'cart', component: CartComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{ useHash: false, initialNavigation: 'enabled' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
+import { HomeComponent } from './views/home/home.component';
+import { RegisterComponent } from './views/register/register.component';
+import { CartComponent } from './views/cart/cart.component';
+import { AccountComponent } from './views/account/account.component';
+import { CategoryComponent } from './views/category/category.component';
+import { ProductComponent } from './views/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent,
+    CartComponent,
+    AccountComponent,
+    CategoryComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
